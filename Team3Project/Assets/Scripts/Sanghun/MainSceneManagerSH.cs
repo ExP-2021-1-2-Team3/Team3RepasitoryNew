08f8cb1e95ccf84b7ea93dafb59dd323c96db0f0
Wadaha <63825388+Jeong-Sanghun@users.noreply.github.com>
2021-05-27 00:33:06 +0900

    글리치 추가

--- Team3Project/Assets/Scripts/Sanghun/MainSceneManagerSH.cs
+++ Team3Project/Assets/Scripts/Sanghun/MainSceneManagerSH.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainSceneManagerSH : MonoBehaviour
 {
@@ -13,9 +14,13 @@ public class MainSceneManagerSH : MonoBehaviour
     GameObject maskParent;
     [SerializeField]
     GameObject normalBackGround;
+
+    [SerializeField]
+    GameObject[] glitchArray;
     void Start()
     {
         loadManager = LoadManagerSH.singleTon;
+        MakeGlitch();
     }
 
     public void GameStartButton()
@@ -34,16 +39,48 @@ public class MainSceneManagerSH : MonoBehaviour
     void MakeGlitch()
     {
         nextStage = loadManager.nextStage;
+        if(nextStage == 1)
+        {
+            return;
+        }
         if (nextStage > 4)
         {
             normalBackGround.SetActive(false);
+            
             return;
         }
-        for(int i = 0; i < 8*(nextStage-1); i++)
+        StartCoroutine(GlitchCoroutine(nextStage - 1));
+    }
+
+    IEnumerator GlitchCoroutine(int index)
+    {
+        SpriteRenderer[] childrens = new SpriteRenderer[3];
+        for (int i = 0; i < index; i++)
         {
-            maskParent.transform.GetChild(i).gameObject.SetActive(true);
+            childrens[i] = glitchArray[i].GetComponent<SpriteRenderer>();
+            childrens[i].gameObject.SetActive(true);
         }
-        
+        while (true)
+        {
+            for(int i = 0; i < index; i++)
+            {
+                childrens[i].color = new Color(1, 1, 1, Random.Range(0, 1f));
+                for (int j = 0; j < 8 * (nextStage - 1); j++)
+                {
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        continue;
+                    }
+                    maskParent.transform.GetChild(j).gameObject.SetActive(true);
+                }
+                yield return new WaitForSeconds(0.3f);
+                for (int j = 0; j < 8 * (nextStage - 1); j++)
+                {
+                    maskParent.transform.GetChild(j).gameObject.SetActive(false);
+                }
+            }
+
+        }
     }
 
     // Update is called once per frame
