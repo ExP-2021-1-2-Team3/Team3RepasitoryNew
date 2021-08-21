1a3edb89e84a1f822905e958e4ecb79f743bf02b
BaekAhjin <gohavanaaa@naver.com>
2021-08-21 20:18:01 +0900

    수정 사항

---
+++ Team3Project/Assets/Scripts/Ahjin/Background.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public PhaseManager phaseManager;
+    public GameObject bg1, bg2, bg3, bg4;
+
+    void Update()
+    {
+        if (phaseManager.phase == 0)
+        {
+            bg1.gameObject.SetActive(true);
+            bg2.gameObject.SetActive(false);
+            bg3.gameObject.SetActive(false);
+            bg4.gameObject.SetActive(false);
+        }
+
+        if (phaseManager.phase == 1)
+        {
+            bg1.gameObject.SetActive(false);
+            bg2.gameObject.SetActive(true);
+            bg3.gameObject.SetActive(false);
+            bg4.gameObject.SetActive(false);
+        }
+
+        if (phaseManager.phase == 2)
+        {
+            bg1.gameObject.SetActive(false);
+            bg2.gameObject.SetActive(false);
+            bg3.gameObject.SetActive(true);
+            bg4.gameObject.SetActive(false);
+        }
+
+        if (phaseManager.phase == 3)
+        {
+            bg1.gameObject.SetActive(false);
+            bg2.gameObject.SetActive(false);
+            bg3.gameObject.SetActive(false);
+            bg4.gameObject.SetActive(true);
+        }
+
+        if (phaseManager.phase == 4)
+        {
+            bg1.gameObject.SetActive(false);
+            bg2.gameObject.SetActive(false);
+            bg3.gameObject.SetActive(false);
+            bg4.gameObject.SetActive(true);
+        }
+    }
+}
