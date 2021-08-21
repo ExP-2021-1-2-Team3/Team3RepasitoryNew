defca9ce01157ec6b308a5cdb4e246fc1c042d37
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2021-08-21 15:30:10 +0900

    파티클 추가
    그리고 이스터에그 하나...

--- Team3Project/Assets/Scripts/Heesu/LetterHS.cs
+++ Team3Project/Assets/Scripts/Heesu/LetterHS.cs
@@ -7,17 +7,45 @@ using UnityEngine.UI;
 public class LetterHS : MonoBehaviour
 {
     //Inspector에서 this_letter값을 보실 수 있습니다.
+    [Header("Set In inspector")]
+    public float gravity;
+
+    //파티클을 끄고 싶으시다면 fasle를 넣어주세요.
+    public static bool enable_particle = false;
+
     [Header("Set In Runtime")]
     public char this_letter;
     private const float destroyLimit = -15.0f;
     private bool game_is_not_over = true;
 
+    private Rigidbody2D rigidBody;
+    private BoxCollider2D colision;
+    private ParticleSystem particle;
+    private SpriteRenderer image;
+
+    private bool is_animating = false;
+    private static char prev_char = '?';
+
     //다시 화면에 등장할 때 불리는 함수
     public void ResetByChar(char setChar)
     {
+        //클릭이 전에 된 개체가 처리가 끝나지 않은채로 바로 초기화하는 일은 방지.
+        if(is_animating && enable_particle)
+            return;
+        
         //텍스트 매쉬 하고 리지드 바디 가져오기
-        Rigidbody2D rigidBody = gameObject.GetComponent<Rigidbody2D>();
-        SpriteRenderer image = gameObject.GetComponent<SpriteRenderer>();
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        particle = gameObject.GetComponent<ParticleSystem>();
+        colision = gameObject.GetComponent<BoxCollider2D>();
+        image = gameObject.GetComponent<SpriteRenderer>();
+
+        if(enable_particle){
+            //파티클 시스템, 콜리전, 이미지 활성화            
+            particle.Play(true);
+            colision.enabled = true;
+            image.color = Color.white;
+            rigidBody.gravityScale = gravity;
+        }
 
         //랜덤 위치 / 기울기 선정
         gameObject.transform.position = new Vector3(Random.Range(-7.5f, 7.5f), 9, 0);
@@ -27,14 +55,23 @@ public class LetterHS : MonoBehaviour
         image.sprite = Resources.Load<Sprite>(setChar.ToString()) as Sprite;
         this_letter = setChar;
         gameObject.SetActive(true);
+        
+        if(!enable_particle)
+            particle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
     }
 
     //이걸 이용하면 사용자가 글자를 클릭했는지 알 수 있음.
     void OnMouseDown()
     {
         if(game_is_not_over){
+            if(prev_char == 'X' && this_letter == 'X')
+                enable_particle = true;
+            prev_char = this_letter;
             LetterUiManagerHS.GetInstance().InputChar(this_letter);
-            gameObject.SetActive(false);
+            if(enable_particle)
+                StartCoroutine(Fade());
+            else
+                gameObject.SetActive(false);
         }
     }
 
@@ -52,6 +89,8 @@ public class LetterHS : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(!enable_particle)
+            particle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
     }
 
     // Update is called once per frame
@@ -60,7 +99,27 @@ public class LetterHS : MonoBehaviour
         //현시점에서는 그다지 필요없는 코드
         //어짜피 리스트 안에서 순회당하면서 다시 불려옴
         if(this.IsOutOfRange()){
-            gameObject.SetActive(false);
+            if(enable_particle)
+                StartCoroutine(Fade());
+            else
+                gameObject.SetActive(false);
         }
+    }
+
+    IEnumerator Fade(){
+        is_animating = true;
+        WaitForEndOfFrame delay = new WaitForEndOfFrame();
+        colision.enabled = false;
+        //rigidBody.velocity = Vector2.zero;
+        //rigidBody.gravityScale = 0.0f;
+        particle.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        image.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+        while(particle.particleCount > 0){
+            yield return delay;
+        }
+
+        gameObject.SetActive(false);
+        is_animating = false;
     }
 }
